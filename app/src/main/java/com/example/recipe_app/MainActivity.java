53db658d884c54d1a644fb7fac8cc2b134882050
James Ballard <jamesballard87@hotmail.com>
2020-11-27 19:03:02 -0600

    button progress

--- app/src/main/java/com/example/recipe_app/MainActivity.java
+++ app/src/main/java/com/example/recipe_app/MainActivity.java
@@ -2,26 +2,23 @@ package com.example.recipe_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button BtnActivityOne;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BtnActivityOne = findViewById(R.id.BtnActivityOne);
+    }
 
-        BtnActivityOne.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d("MYTAG", "onClick: Was Clicked ");
-
-            }
-        });
+    public void openRecipe(View view) {
+        Intent intent = new Intent(this, RecipesActivityTwo.class);
+        startActivity(intent);
     }
 }
+
