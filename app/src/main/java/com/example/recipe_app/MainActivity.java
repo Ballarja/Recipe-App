1a32832c18af3fd91d1ef9ca356f3a0c5755957d
Ballarja <54514754+Ballarja@users.noreply.github.com>
2020-12-02 14:22:56 -0600

    working on toolbar

--- app/src/main/java/com/example/recipe_app/MainActivity.java
+++ app/src/main/java/com/example/recipe_app/MainActivity.java
@@ -3,8 +3,9 @@ package com.example.recipe_app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
-
+import android.widget.Toolbar;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,14 +18,22 @@ import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
 
+   Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
 
-        public void openRecipe(View view) {
+    @Override
+    public boolean  onCreateOptionsMenu(Menu menu){
+    }
+
+    public void openRecipe(View view) {
             Intent intent = new Intent(this, RecipesActivityTwo.class);
             startActivity(intent);
         }
@@ -50,3 +59,16 @@ public class MainActivity extends AppCompatActivity {
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
