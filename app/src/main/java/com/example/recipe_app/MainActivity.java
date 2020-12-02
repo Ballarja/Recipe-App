bbf58748699bdd7336f7ab992a4df88ac48bdce8
James Ballard <jamesballard87@hotmail.com>
2020-12-01 20:10:50 -0600

    Working on network between app and internet through search view function (" added implementation 'com.loopj.android:android-async-http:1.4.9' to --build.gradle--"

--- app/src/main/java/com/example/recipe_app/MainActivity.java
+++ app/src/main/java/com/example/recipe_app/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.recipe_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import cz.msebera.android.httpclient.Header;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +24,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void openRecipe(View view) {
-        Intent intent = new Intent(this, RecipesActivityTwo.class);
-        startActivity(intent);
-    }
-}
+        public void openRecipe(View view) {
+            Intent intent = new Intent(this, RecipesActivityTwo.class);
+            startActivity(intent);
+        }
 
+        public void searchRecipe(View v) {
+
+        String url = "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata";
+
+        new AsyncHttpClient().get(url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                String string = new String(responseBody);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+            }
+        });
+        }
+
+
+
+
+}
